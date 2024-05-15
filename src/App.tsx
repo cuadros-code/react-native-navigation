ab815a0ba70c42cb4449236dd84e23d32153e46a
kevin cuadros <kevindavidcuadros@gmail.com>
2024-05-14 19:30:44 -0500

    create Stack Navigator

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
+import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { HomeScreen } from './presentation/screens/home/HomeScreen';
-
-const Stack = createNativeStackNavigator();
+import { StackNavigator } from './presentation/routes/StackNavigator';
 
 export const App = () => {
   
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <StackNavigator />
     </NavigationContainer>
   );
 }
