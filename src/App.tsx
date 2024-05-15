b9e7cd951d3997e1ebc84f07f90069b8b0ad9909
kevin cuadros <kevindavidcuadros@gmail.com>
2024-05-14 19:13:37 -0500

    chore: Update react-native version and add new dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import { SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen } from './presentation/screens/home/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 export const App = () => {
   
   return (
-    <SafeAreaView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -2,8 +2,8 @@ import { Text, View } from 'react-native'
 
 export const HomeScreen = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
   )
 }
