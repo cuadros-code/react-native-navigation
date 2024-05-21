df9ee0237eaffed568a16a452fa50e305c02fa99
kevin cuadros <kevindavidcuadros@gmail.com>
2024-05-21 18:48:57 -0500

    ajuste de navegacion

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,20 @@
-import { Text, View } from 'react-native'
+import { Pressable, Text, View } from 'react-native'
+import { globalStyles } from '../../theme/theme'
+import { useNavigation } from '@react-navigation/native'
 
 export const HomeScreen = () => {
+
+  const navigation = useNavigation()
+
+  const onNavigate = () => {
+    navigation.navigate('Products' as never)
+  }
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <View style={globalStyles.container}>
+      <Pressable onPress={onNavigate} style={globalStyles.primaryColor}>
+        <Text style={globalStyles.buttonText}>Productos</Text>
+      </Pressable>
     </View>
   )
 }
