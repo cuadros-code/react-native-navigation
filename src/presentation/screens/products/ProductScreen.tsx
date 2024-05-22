0239f3aa66b07c2a68bd0a918f623c8490fc53be
kevin cuadros <kevindavidcuadros@gmail.com>
2024-05-22 16:49:31 -0500

    end stack section

--- src/presentation/screens/products/ProductScreen.tsx
+++ src/presentation/screens/products/ProductScreen.tsx
@@ -1,9 +1,25 @@
+import { RouteProp, useNavigation, useRoute } from '@react-navigation/native'
 import { Text, View } from 'react-native'
+import { RootStackProps } from '../../routes/StackNavigator'
+import { useEffect } from 'react'
 
 export const ProductScreen = () => {
+
+  const { id, name } = useRoute<RouteProp<RootStackProps, 'Product'>>().params
+  const navigation = useNavigation()
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: name
+    })
+  }, [])
+  
+
   return (
     <View>
       <Text>Product Screen</Text>
+      <Text>id: {id}</Text>
+      <Text>name: {name}</Text>
     </View>
   )
 }
--- src/presentation/screens/settings/SettingsScreen.tsx
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,9 +1,23 @@
 import { Text, View } from 'react-native'
+import { globalStyles } from '../../theme/theme'
+import { PrimaryButton } from '../../components/PrimaryButton'
+import { StackActions, useNavigation } from '@react-navigation/native'
 
 export const SettingsScreen = () => {
+
+  const navigation = useNavigation()
+
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={globalStyles.container}>
+      <Text style={{ marginBottom: 10 }}>Settings Screen</Text>
+      <PrimaryButton
+        label='Atras'
+        onPress={ () => navigation.goBack() }
+      />
+      <PrimaryButton
+        label='Home'
+        onPress={ () => navigation.dispatch( StackActions.popToTop() ) }
+      />
     </View>
   )
 }
