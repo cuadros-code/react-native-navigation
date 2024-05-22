21d828d83635169e5308d5b3c637bc47d8031839
kevin cuadros <kevindavidcuadros@gmail.com>
2024-05-22 17:00:17 -0500

    implement Drawer navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-import { StackNavigator } from './presentation/routes/StackNavigator';
+import { DrawerNavigator } from './presentation/routes/DrawerNavigator';
 
 export const App = () => {
   
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <DrawerNavigator />
     </NavigationContainer>
   );
 }
